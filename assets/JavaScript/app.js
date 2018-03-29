07ed63c53e0cdf748000e80ad59991fc1b627bcf
rbenites <rbenites115@gmail.com>
2018-03-29 10:05:08 -0700

    updated HTML, files, and JS with api call

---
+++ assets/JavaScript/app.js
@@ -0,0 +1,9 @@
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=ssisEaruVrcF1igPEGOT1LEyzqZkCvKZ";
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function(response) {
+      console.log(response);
+    });
+
