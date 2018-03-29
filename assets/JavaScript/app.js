4630b2bb5756390168cf7f194f645b5f0ec8b031
rbenites <rbenites115@gmail.com>
2018-03-29 10:23:13 -0700

    no major changes

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,3 +1,5 @@
+window.onload = function () {
+
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=ssisEaruVrcF1igPEGOT1LEyzqZkCvKZ";
 
     $.ajax({
@@ -7,3 +9,5 @@ var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=ssisEaruVrcF1igPE
       console.log(response);
     });
 
+
+  };
